03738b8f8f87c60e7bb4705a5709579004054434
jernej.halozan <jernej.halozan@d77d96cf-bc4d-0410-95e8-7f58e8f99bdb>
2008-05-31 19:25:30 +0000

    Dodan header dodatoteke. Povsod po programu sem zamenjal char* z string za delo nad nizi sporočil. Popravil zamike.

--- trunk/src/messages.h
+++ trunk/src/messages.h
@@ -1,68 +1,73 @@
 /*
-  codename: Pac-Game
-  Aljosa Osep 2007
+ * Codename: xSoko
+ * File: messages.h
+ *
+ * Summary:
+ * Includes PMessage class implementation
+ *
+ * Author: Aljosa Osep 2008
+ * Changes:
+ * Jernej Haložan 2008
 */
 
 #ifndef __MESSAGES_H
 #define __MESSAGES_H
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 namespace PacGame
 {
-          namespace GameClasses
-          {
-                    class PMessages
-                    {
-					public:
-						void initMessage(char *string, bool isSuccess)
-						{
-							if(isSuccess)
-								cout<<"Initializing "<<string<<" ........... OK"<<endl;
-							else
-								cout<<"Initializing "<<string<<" ........... FAILED"<<endl;
-						}
+    namespace GameClasses
+    {
+        class PMessages
+        {
+	public:
+            void initMessage(string text, bool isSuccess)
+            {
+                if(isSuccess)
+                    cout<<"Initializing "<<text<<" ........... OK"<<endl;
+                else
+                    cout<<"Initializing "<<text<<" ........... FAILED"<<endl;
+            }
 
-						void errorMessage(char *string)
-						{
-							cout<<"Error: "<<string<<endl;
-						}
+            void errorMessage(string text)
+            {
+                cout<<"Error: "<<text<<endl;
+            }
 
-						void errorIndexOutOfRange()
-						{
-							cout<<"Error: index out of range!"<<endl;
-						}
+            void errorIndexOutOfRange()
+            {
+                cout<<"Error: index out of range!"<<endl;
+            }
 
-						void errorUnknown()
-						{
-							cout<<"Error: unknown!"<<endl;
-						}
+            void errorUnknown()
+            {
+                cout<<"Error: unknown!"<<endl;
+            }
 
 
-						void infoMessage(char *string)
-						{
-							cout<<"Info: "<<string<<endl;
-						}
+            void infoMessage(string text)
+            {
+                cout<<"Info: "<<text<<endl;
+            }
 
-						void infoTexture(const char *string)
-						{
-							cout<<"Info: Tga file "<<string<<" was successfully loaded."<<endl;
-						}
+            void infoTexture(string text)
+            {
+                cout<<"Info: Tga file "<<text<<" was successfully loaded."<<endl;
+            }
 
-						void mainTitleMessage()
-						{
-							cout<<"======================================"<<endl;
-							cout<<"==        CODENAME: xSoko           =="<<endl;
-							cout<<"==          Console output          =="<<endl;
-							cout<<"======================================"<<endl;
-							cout<<endl;
-						}
-                    };
-                    
-              //      PMessages PStatus;
-          }
+            void mainTitleMessage()
+            {
+                cout<<"======================================"<<endl;
+                cout<<"==        CODENAME: xSoko           =="<<endl;
+                cout<<"==          Console output          =="<<endl;
+                cout<<"======================================"<<endl;
+                cout<<endl;
+            }
+        };
+    }
 }
 
 #endif
