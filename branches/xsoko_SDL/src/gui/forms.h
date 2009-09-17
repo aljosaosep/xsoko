5c636337342c77e13f9871d207acb84dff99ca55
jernej.skrabec <jernej.skrabec@d77d96cf-bc4d-0410-95e8-7f58e8f99bdb>
2009-09-17 20:08:06 +0000

    pozabil dodat 2 fajla

---
+++ branches/xsoko_SDL/src/gui/forms.h
@@ -0,0 +1,123 @@
+/*
+ * codename: xSoko
+ * Copyright (C) Aljosa Osep, Jernej Skrabec, Jernej Halozan 2008 <aljosa.osep@gmail.com, jernej.skrabec@gmail.com, jernej.halozan@gmail.com>
+ *
+ * xSoko project is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * xSoko project is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* 
+ * File:   forms.h
+ * Author: jernej
+ *
+ * Created on Torek, 15 september 2009, 20:15
+ */
+
+#ifndef _FORMS_H
+#define	_FORMS_H
+
+#include "window.h"
+#include "button.h"
+#include "listbox.h"
+#include "scrollbar.h"
+#include "editbox.h"
+#include "checkbox.h"
+#include "text.h"
+
+class MainMenu : public Window {
+private:
+    Button* btnCampaign;
+    Button* btnFreeplay;
+    Button* btnOptions;
+    Button* btnCredits;
+    Button* btnExit;
+
+    //another windows
+    Window* freeplay;
+    Window* options;
+    Window* credits;
+private:
+    void initializeComponents();
+    void btnCampaignClick(Component* sender);
+    void btnFreeplayClick(Component* sender);
+    void btnOptionsClick(Component* sender);
+    void btnCreditsClick(Component* sender);
+    void btnExitClick(Component* sender);
+public:
+    MainMenu();
+};
+
+class FreeplayWnd: public Window {
+private:
+    ListBox* lstLevels;
+    Button* btnBack;
+    Button* btnPlay;
+
+    //main menu
+    Window* mainMenu;
+private:
+    void initializeComponents();
+    void btnBackClick(Component* sender);
+    void btnPlayClick(Component* sender);
+    void lstLevelsKeyUp(Component* sender, int key);
+public:
+    FreeplayWnd();
+};
+
+class OptionsWnd : public Window {
+private:
+    Button* btnApply;
+    Button* btnBack;
+    ListBox* lstModes;
+    CheckBox* chkFullscreen;
+    Text* label1;
+    Text* label2;
+
+    //main menu
+    Window* mainMenu;
+private:
+    void initializeComponents();
+    void btnApplyClick(Component* sender);
+    void btnBackClick(Component* sender);
+public:
+    OptionsWnd();
+};
+
+class CreditsWnd : public Window {
+private:
+    Text* label;
+    Button* btnOK;
+
+    //main menu
+    Window* mainMenu;
+private:
+    void intializeComponents();
+    void btnOKClick(Component* sender);
+public:
+    CreditsWnd();
+};
+
+class GameMenu : public Window {
+private:
+    Button* btnSave;
+    Button* btnReset;
+    Button* btnExit;
+private:
+    void initializeComponents();
+    void btnResetClick(Component* sender);
+    void btnExitClick(Component* sender);
+public:
+    GameMenu();
+};
+
+#endif	/* _FORMS_H */
+
