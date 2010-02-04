5b3ad0460f2fa20a9e0120c3fbba180d6a484a93
aljosa.osep <aljosa.osep@d77d96cf-bc4d-0410-95e8-7f58e8f99bdb>
2010-02-04 18:38:36 +0000

    Podpora LWO (loader, izris, dodan krogla.lwo model)

---
+++ src/LWObject.h
@@ -0,0 +1,55 @@
+/* 
+ * File:   LWObject.h
+ * Author: aljosa
+ *
+ * Created on February 4, 2010, 7:17 PM
+ */
+
+#ifndef _LWOBJECT_H
+#define	_LWOBJECT_H
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+#include "CommonStructures.h"
+
+using namespace PacGame::Structures;
+using namespace std;
+
+namespace PacGame
+{
+    namespace LWFormat
+    {
+        class CLWObject
+        {
+        public:
+                VECTOR *pts;    // oglisca
+                POLYGON *plys;  // mnogokotniki
+                SURFACE *srfs;  // povrsja
+
+                int numpoints, numpolygons, numsurfaces;
+                int activeSurfIndex;
+
+                CLWObject();
+                virtual ~CLWObject();
+                bool LoadTags(FILE *fp);
+                bool FindChunk(FILE *fp, const char *chunk);
+                bool FindNextChunk(FILE *fp, const char *chunk);
+                int LoadPoints(FILE *fp);
+                int LoadPolygons(FILE *fp);
+                bool LoadSurfaces(FILE *fp);
+                bool ReadInt(FILE *fp, int &i);
+                bool ReadFloat(FILE * fp, float &f);
+                int AddPolygon(FILE * fp);
+                bool ReadShort(FILE * fp, unsigned short &s);
+                bool ReadSubChunk(char *buf, unsigned short &s, FILE *fp);
+                void CalcNormals();
+                void Clear();
+                bool Load(char *path);
+        };
+    }
+}
+
+#endif	/* _LWOBJECT_H */
+
