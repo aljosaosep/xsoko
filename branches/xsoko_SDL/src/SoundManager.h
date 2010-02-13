17c60cacce9e12292b3ecd3013f68e70734a4670
jernej.halozan <jernej.halozan@d77d96cf-bc4d-0410-95e8-7f58e8f99bdb>
2010-02-13 10:07:33 +0000

    sound manager finalization - code refactoring, music player finalization

--- branches/xsoko_SDL/src/SoundManager.h
+++ branches/xsoko_SDL/src/SoundManager.h
@@ -14,35 +14,41 @@
 #include <string>
 using namespace std;
 
-class SoundManager {
-    
-    int channel0;           //effects channel
-    int channel1;           //music channel
-    int audio_rate;         //Frequency of audio playback
-    Uint16 audio_format;    //Format of the audio we're playing
-    int audio_channels;     //2 channels = stereo
-    int audio_buffers;      //Size of the audio buffers in memory
+namespace PacGame
+{
+    namespace GameClasses
+    {
 
-    map<string,Mix_Chunk*> effects;
-    map<string,Mix_Music*> musics;
+        class SoundManager {
 
-    bool musicStatus;       // true - music is playing, false - no backgound music
-    string currentMusic;    // name of currently playing music
+            int channel0;           //effects channel
+            int channel1;           //music channel
+            int audio_rate;         //Frequency of audio playback
+            Uint16 audio_format;    //Format of the audio we're playing
+            int audio_channels;     //2 channels = stereo
+            int audio_buffers;      //Size of the audio buffers in memory
 
-public:
-    SoundManager();
-    SoundManager(const SoundManager& orig);
-    virtual ~SoundManager();
-    void loadEffect(string name, string path);
-    void playEffect(string name);
-    void loadMusic(string name, string path);
-    void playMusic(string name);
-    void playMusic();
-    void stopMusic();
-    bool getMusicStatus();
-    void playNextMusic();
-    void playPreviousMusic();
-};
+            map<string,Mix_Chunk*> effects;
+            map<string,Mix_Music*> musics;
 
+            bool musicStatus;       // true - music is playing, false - no backgound music
+            string lastMusic;       // name of last played music
+
+        public:
+            SoundManager();
+            SoundManager(const SoundManager& orig);
+            virtual ~SoundManager();
+            void loadEffect(string name, string path);
+            void playEffect(string name);
+            void loadMusic(string name, string path);
+            void playMusic(string name);
+            void playMusic();
+            void stopMusic();
+            bool getMusicStatus();
+            void playNextMusic();
+            void playPreviousMusic();
+        };
+    }
+}
 #endif	/* _SOUNDMANAGER_H */
 
